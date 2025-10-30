848fa4aef918f7481f492ccdec6b0b5f2330cf39
Atharvwasthere <singhatharv1919@gmail.com>
2025-10-30 17:04:24 +0530

    fix(db):add retry logic for db connection

--- Backend/src/config/prisma.ts
+++ Backend/src/config/prisma.ts
@@ -1,2 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
+
+async function ensureDBConnection() {
+  for (let i = 0; i < 5 ; i++){
+    try{
+      await prisma.$connect();
+      console.log("Database connection established");
+      break;
+    } catch(err){
+      console.warn(`Retrying connection attempt ${i+1}`);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      console.log(`Connection attempt ${i+1} failed`);
+    }
+  }
+}
+
+ensureDBConnection();
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { createServer } from "./server.js";
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 const app = createServer();
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`🚀 Orion backend running at http://localhost:${port}`);
 });
